80af9c3fa4c7a50980b00e31f3e18082881ccec3
milk717 <robolindasoo@gmail.com>
2023-01-10 22:19:45 +0900

    design: header, footer 위치 고정

--- src/resources/style/GlobalStyle.js
+++ src/resources/style/GlobalStyle.js
@@ -6,6 +6,18 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  
+  header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+  
+  footer{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
 `;
 
 export default GlobalStyle;
