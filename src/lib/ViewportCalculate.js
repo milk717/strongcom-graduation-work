72ef5c17c152e59047275de79fbfa338556fcf31
milk717 <robolindasoo@gmail.com>
2023-01-10 22:18:49 +0900

    feat: viewport 계산 util 함수 수정

--- src/lib/ViewportCalculate.js
+++ src/lib/ViewportCalculate.js
@@ -1,7 +1,7 @@
-export const viewHeightCalc = (px,{height = 1024}) =>{
+export const viewHeightCalc = (px,{height = 760}) =>{
     return `${(px * 100) / height}vh`;
 }
 
-export const viewWidthCalc = (px,{width = 1440}) =>{
+export const viewWidthCalc = (px,{width = 360}) =>{
     return `${(px * 100) / width}vw`;
 }
