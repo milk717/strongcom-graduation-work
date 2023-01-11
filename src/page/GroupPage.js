2accd0e7f4b415efc4b5f88ff2b21537ecec39eb
milk717 <robolindasoo@gmail.com>
2023-01-11 13:40:25 +0900

    feat: groupList 만듦.

--- src/page/GroupPage.js
+++ src/page/GroupPage.js
@@ -1,6 +1,9 @@
+import GroupList from "../components/GroupList";
+
 export default function GroupPage(){
     return(
         <>
+            <GroupList/>
         </>
     )
 }
--- src/resources/style/theme.js
+++ src/resources/style/theme.js
@@ -3,8 +3,8 @@ const theme = {
     color:{
         primary: '#0F2859',
         primaryLight: '#37476b',
-        primaryDark: '#00001b',
         secondary: '#7787A6',
+        secondaryLight: 'rgba(119,135,166,0.56)',
         background: '#F5F5F6',
         accent:'#082040',
         black:'#000',
