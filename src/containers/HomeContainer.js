ba9d09a4293328ab58f2dd2190cbf25f31efc50d
milk717 <robolindasoo@gmail.com>
2023-02-01 22:50:15 +0900

    feat: 홈화면에서 오늘의 리마인더 조회 가능

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,13 +1,19 @@
-import TaskList from "../components/TaskList";
 import HomeBox from "../components/HomeBox";
 import HomeList from "../components/HomeList";
 import {dummyNoticeData, dummyTaskData} from "../resources/dummy";
+import {useSelector} from "react-redux";
+import {useGetReminderQuery} from "../api/jsonApi";
 
 export default function HomeContainer(){
+    const {data, error, isLoading} = useGetReminderQuery();
+
+    if(isLoading) return 'Loading...'
+    if(error)return 'Error'
+
     return(
         <>
             <HomeBox boxTitle={'Today\'s Reminder'}>
-                <HomeList listData={dummyTaskData}/>
+                <HomeList listData={data}/>
             </HomeBox>
             <HomeBox boxTitle={'Notification'}>
                 <HomeList listData={dummyNoticeData}/>
